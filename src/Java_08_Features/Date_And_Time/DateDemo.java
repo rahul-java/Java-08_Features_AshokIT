b0f3a7c282b99331f05144ef4002d7266f12f14b
rahul <rp201291.com>
2023-09-01 02:15:29 +0530

    date modified Commit

--- src/Java_08_Features/Date_And_Time/DateDemo.java
+++ src/Java_08_Features/Date_And_Time/DateDemo.java
@@ -8,32 +8,32 @@ public class DateDemo {
 	public static void main(String[] args) throws Exception {
 
 		Date date = new Date();
-		System.out.println(date);
+		System.out.println(date); //Fri Sep 01 02:12:06 IST 2023
 
 		// format :: dd/MM/yyyy
 
 		// Converting Date to String
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy"); // indian date format
 		String format = simpleDateFormat.format(date);
-		System.out.println(format);
+		System.out.println(format);  //01/09/2023
 
 		SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("dd-MM-yyyy"); // indian date format
 		String format1 = simpleDateFormat1.format(date);
-		System.out.println(format1);
+		System.out.println(format1);  //01-09-2023
 
 		SimpleDateFormat simpleDateFormat2 = new SimpleDateFormat("MM/dd/yyyy");
 		String format2 = simpleDateFormat2.format(date);
-		System.out.println(format2);
+		System.out.println(format2);  //09/01/2023
 
 		// Converting String to Date
 		SimpleDateFormat simpleDateFormat3 = new SimpleDateFormat("yyyy-MM-dd");
 		Date parsedDate = simpleDateFormat3.parse("2023-08-29");  //parse method convert string to date format
-		System.out.println(parsedDate);
+		System.out.println(parsedDate);  //Tue Aug 29 00:00:00 IST 2023
 
 		// Converting String to Date
 		SimpleDateFormat simpleDateFormat4 = new SimpleDateFormat("dd-MM-yyyy");
 		Date parsedDate1 = simpleDateFormat4.parse(format1); //here format1 is a String of "dd-MM-yyyy" format
-		System.out.println(parsedDate1);
+		System.out.println(parsedDate1);  //Fri Sep 01 00:00:00 IST 2023
 
 	}
 }
--- src/Java_08_Features/Date_And_Time/SQLDateDemo.java
+++ src/Java_08_Features/Date_And_Time/SQLDateDemo.java
@@ -9,13 +9,13 @@ public class SQLDateDemo {
 	public static void main(String[] args) throws Exception {
 
 		Date date = new Date(20230830);
-		System.out.println(date);
+		System.out.println(date);  //1970-01-01
 
 		// format :: dd/MM/yyyy
 
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy"); 
 		String format = simpleDateFormat.format(date);
-		System.out.println(format);
+		System.out.println(format);  //01/01/1970
 
 		
 
